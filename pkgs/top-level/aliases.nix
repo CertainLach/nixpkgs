944f2d7945cf0196419a13eaf4b270c36a30f565
Yaroslav Bolyukin <iam@lach.pw>
2021-06-18 21:32:56 +0300

    plasma5: remove ksysguard
    It was deprecated in favour of system-monitor
    
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1107,7 +1107,7 @@ mapAliases ({
     kgamma5
     kinfocenter
     kmenuedit
-    kscreen kscreenlocker ksshaskpass ksysguard
+    kscreen kscreenlocker ksshaskpass
     kwallet-pam kwayland-integration kwin kwrited
     milou
     oxygen
