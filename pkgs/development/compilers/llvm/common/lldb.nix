6000f512da862f52716455bca2cc9c4b541d9e71
Semion Nadezhdin <me@sg-studio.ru>
2023-08-28 21:42:39 +0500

    llvmPackages_16.lldb: Hack to fix dynamic linking mishap
    Not really proud of this. Very hacky, but I can't figure out why rpath entry become missing after introduction of 251c557 , and that is the only way I could do it without hindering any potential improvements. I guess working, but hacky is better then pretending to be working and failing, at least for a while.

--- pkgs/development/compilers/llvm/common/lldb.nix
+++ pkgs/development/compilers/llvm/common/lldb.nix
@@ -172,6 +172,9 @@ stdenv.mkDerivation (rec {
   passthru.vscodeExtPublisher = "llvm";
   passthru.vscodeExtUniqueId = "llvm-org.${vscodeExt.name}-${vscodeExt.version}";
 
+  # TODO: Hack to make dynamic linking work again. Find a better way to do this using the build-system machinery
+  postFixup = if (stdenv.isLinux && lib.versionAtLeast release_version "16") then "patchelf --add-rpath ${libclang.lib}/lib $out/bin/.lldb-wrapped" else null;
+
   meta = llvm_meta // {
     homepage = "https://lldb.llvm.org/";
     description = "A next-generation high-performance debugger";
@@ -207,6 +210,7 @@ stdenv.mkDerivation (rec {
 
   postPatch = null;
   postInstall = null;
+  postFixup = null;
 
   outputs = [ "out" ];
 
