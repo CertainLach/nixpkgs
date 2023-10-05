d94db425b3657e6dc311c12dc8156c0ac2e1922f
Yaroslav Bolyukin <iam@lach.pw>
2023-10-05 23:27:10 +0200

    fixup lldb

--- pkgs/development/compilers/llvm/common/lldb.nix
+++ pkgs/development/compilers/llvm/common/lldb.nix
@@ -164,7 +164,11 @@ stdenv.mkDerivation (rec {
   passthru.vscodeExtUniqueId = "llvm-org.lldb-vscode-0.1.0";
 
   # TODO: Hack to make dynamic linking work again. Find a better way to do this using the build-system machinery
-  postFixup = if (stdenv.isLinux && lib.versionAtLeast release_version "16") then "patchelf --add-rpath ${libclang.lib}/lib $out/bin/.lldb-wrapped" else null;
+  postFixup = if (stdenv.isLinux && lib.versionAtLeast release_version "16") then ''
+    for bin in .lldb-wrapped lldb-server; do
+      patchelf --add-rpath ${libclang.lib}/lib $out/bin/$bin
+    done
+  '' else null;
 
   meta = llvm_meta // {
     homepage = "https://lldb.llvm.org/";
