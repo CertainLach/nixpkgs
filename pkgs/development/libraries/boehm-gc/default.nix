86f2a8a6f153aeb47eb6b860b563c7728fd03ac7
Yaroslav Bolyukin <iam@lach.pw>
2023-11-01 13:22:06 +0100

    boehm-gc: disable check on aarch64

--- pkgs/development/libraries/boehm-gc/default.nix
+++ pkgs/development/libraries/boehm-gc/default.nix
@@ -47,7 +47,7 @@ stdenv.mkDerivation (finalAttrs: {
     ];
 
   # `gctest` fails under emulation on aarch64-darwin
-  doCheck = !(stdenv.isDarwin && stdenv.isx86_64);
+  doCheck = !(stdenv.isDarwin && stdenv.isx86_64 || stdenv.isLinux && stdenv.isaarch64);
 
   enableParallelBuilding = true;
 
