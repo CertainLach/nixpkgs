41dec23aae54093032f8a09dd8efdd716a9c57b4
Yaroslav Bolyukin <iam@lach.pw>
2024-02-05 10:25:52 +0100

    openai-whisper-cpp: rocmSupport

--- pkgs/tools/audio/openai-whisper-cpp/default.nix
+++ pkgs/tools/audio/openai-whisper-cpp/default.nix
@@ -14,6 +14,9 @@
 , config
 , cudaSupport ? config.cudaSupport
 , cudaPackages ? {}
+
+, rocmSupport ? config.rocmSupport
+, rocmPackages ? {}
 }:
 
 let
@@ -69,6 +72,10 @@ effectiveStdenv.mkDerivation (finalAttrs: {
       libcublas.dev
       libcublas.lib
       libcublas.static
+    ]) ++ lib.optionals rocmSupport (with rocmPackages; [
+      clr
+      hipblas
+      rocblas
     ]);
 
   postPatch = let
@@ -84,6 +91,10 @@ effectiveStdenv.mkDerivation (finalAttrs: {
     WHISPER_COREML_ALLOW_FALLBACK = "1";
   } // lib.optionalAttrs cudaSupport {
     WHISPER_CUBLAS = "1";
+  } // lib.optionalAttrs rocmSupport {
+    WHISPER_HIPBLAS = "1";
+    ROCM_PATH = rocmPackages.clr;
+    GPU_TARGETS = lib.concatStringsSep "," rocmPackages.clr.gpuTargets;
   };
 
   makeFlags = [ "main" "stream" "command" ];
