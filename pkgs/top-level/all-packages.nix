ec908a9f1cc82247b9bbce69ed893e8b48cdf2a3
Yaroslav Bolyukin <iam@lach.pw>
2024-02-20 18:44:30 +0100

    postgresql-neon: init on 16.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26809,6 +26809,9 @@ with pkgs;
     postgresql_14_jit
     postgresql_15_jit
     postgresql_16_jit
+
+    postgresql_16_neon
+    postgresql_16_neon_jit
   ;
   postgresql = postgresql_15.override { this = postgresql; };
   postgresql_jit = postgresql_15_jit.override { this = postgresql_jit; };
